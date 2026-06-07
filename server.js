8aba41a48bed9861d50941cc0322a3ab919668bd
pragnya27441 <pragnyanemani@gmail.com>
2026-06-07 21:52:44 +0530

    Update server.js

--- server.js
+++ server.js
@@ -1,23 +1,23 @@
-
 import express from "express";
 import path from "path";
 import { fileURLToPath } from "url";
 
 const app = express();
-const PORT = 3000;
-
+const PORT = process.env.PORT || 3000;
 
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename);
 
+app.use(express.static(__dirname));
 
-app.use(express.static(path.join(__dirname)));
-
+app.get("/", (req, res) => {
+  res.sendFile(path.join(__dirname, "index.html"));
+});
 
 app.get("/data", (req, res) => {
   res.sendFile(path.join(__dirname, "data.json"));
 });
 
 app.listen(PORT, () => {
-  console.log(`🚀 Server running at http://localhost:${PORT}`);
+  console.log(`Server running on port ${PORT}`);
 });
